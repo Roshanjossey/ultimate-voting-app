1aa73a815aa3aa813b95e9964b3882bf2e7f951d
roshanjossey <roshanjossey@gmail.com>
2016-05-08 22:28:31 +0530

    Start with actions and reducers

---
+++ voting-server/test/reducer_spec.js
@@ -0,0 +1,51 @@
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import reducer from '../src/reducer';
+
+describe ('reducer', () => {
+  it('handles SET_ENTRIES', () => {
+    const initialState = Map();
+    const action = {type: 'SET_ENTRIES', entries: ['Cache']};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      entries: ['Cache']
+    }));
+  });
+
+  it('handles NEXT', () => {
+    const initialState = fromJS({
+      entries: ['Cache', 'Amour']
+    });
+    const action = {type: 'NEXT'}
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Cache', 'Amour']
+      },
+      entries: []
+    }));
+  });
+
+  it('handles VOTE', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Cache', 'Amour']
+      },
+      entries: []
+    });
+    const action = {type: 'VOTE', entry: 'Cache'};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Cache', 'Amour'],
+        tally: {'Cache': 1}
+      },
+      entries: []
+    }));
+
+  });
+});
